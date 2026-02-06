da2f38e132d49d75b0caec22252a056ee3e00370
Sai Ruthvik <ruthviksai42@gmail.com>
2026-02-06 12:03:19 +0530

    Refactor LeakyBucket class and main method

--- LeakyBucket.java
+++ LeakyBucket.java
@@ -1,33 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class LeakyBucket {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter bucket capacity: ");
-        int bucketCapacity = scanner.nextInt();
-        System.out.print("Enter output rate (packets per second): ");
-        int outputRate = scanner.nextInt();
-        System.out.print("Enter the number of packets: ");
-        int numPackets = scanner.nextInt();
-        int[] packetSizes = new int[numPackets];
-        System.out.println("Enter the packet sizes: ");
-        for (int i = 0; i < numPackets; i++) {
-            packetSizes[i] = scanner.nextInt();
-        }
-
-        int currentBucketSize = 0;
-
-        System.out.println("\nPacket Size\tBucket Size\tSent\tRemaining\tStatus");
-        for (int packetSize : packetSizes) {
-            if (currentBucketSize + packetSize <= bucketCapacity) {
-                currentBucketSize += packetSize;
-                System.out.println(packetSize + "\t\t" + currentBucketSize + "\t\t"+Math.min(outputRate,currentBucketSize)+"\t\t"+Math.max(0, currentBucketSize - outputRate)+"\t\tAccepted");
-            } else {
-                System.out.println(packetSize + "\t\t" + currentBucketSize+"\t\t"+Math.min(outputRate,currentBucketSize)+"\t\t"+Math.max(0, currentBucketSize - outputRate)+"\t\tDropped");
-            }
-            currentBucketSize = Math.max(0, currentBucketSize - outputRate);
-        }
-        scanner.close();
-    }
-}
+public class LeakyBucket{
+        public static void main(String args[]){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter the bucket capacity");
+                int bucketcapacity=sc.nextInt();
+                System.out.println("Enter the op rate:");
+                int outputrate=sc.nextInt();
+                System.out.println("Enter the number of packets:");
+                int n=sc.nextInt();
+                int[] packetSizes=new int[n];
+                System.out.println("Enter the packet sizes:");
+                for(int i=0;i<n;i++)
+                        packetSizes[i]=sc.nextInt();
+                int currentbucketsize=0;
+                System.out.println("\n Packet Size \t\t Bucket Size \t\t Sent \t\t Remaining \t\t Status ");
+                for(int packetsize:packetSizes){
+                        if(currentbucketsize+packetsize<=bucketcapacity){
+                                currentbucketsize+=packetsize;
+                                 System.out.println("\n"+packetsize+" \t\t"+currentbucketsize+"\t \t"+Math.min(outputrate,currentbucketsize)+" \t\t"+Math.max(0,currentbucketsize-outputrate)+"\t\tAccepted ");
+                        }
+                        else{
+                               System.out.println("\n"+packetsize+" \t\t"+currentbucketsize+"\t \t"+Math.min(outputrate,currentbucketsize)+" \t\t"+Math.max(0,currentbucketsize-outputrate)+"\t\t Dropped ");
+                        }
+                        currentbucketsize=Math.max(0,currentbucketsize-outputrate);
+                }
+                sc.close();
+        }}
+~
